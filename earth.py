be599f95f42a434563486ff6b88aec33272bc46b
JoaquinKelly <jkelly1@mail.austral.edu.ar>
2025-03-31 15:03:17 -0300

    Update earth.py

--- earth.py
+++ earth.py
@@ -1,3 +1,7 @@
 def earth():
     x = "Bangladesh"
     y = "Barbados"
+    checkeo=(x<y)
+    checkeo1=(x>y)
+    print (f"The result of {x} comes first in the dictionary than {y} is {checkeo}.")
+    print (f"The result of {y} comes first in the dictionary than {x} is {checkeo1}.")
